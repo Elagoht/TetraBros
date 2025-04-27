d2e5d7bc552f83327e90735eabfa06cd5b5b7a5d
EnesBaytekin <enesbaytekin38@gmail.com>
2025-04-27 09:10:47 +0300

    fix: delete debug messages

--- entities/player/scripts/states/die.gd
+++ entities/player/scripts/states/die.gd
@@ -1,13 +1,11 @@
 extends State
 
 func enter() -> void:
-	print("girdim")
 	owner.sprites.play("die")
 	owner.global_position = Vector2(320, 500)
 	$DieTimer.start()
 
 func _on_die_timer_timeout() -> void:
-	print("çıktım")
 	owner.global_position = Vector2(320, -20)
 	owner.velocity = Vector2.ZERO
 	owner.renew_health()
